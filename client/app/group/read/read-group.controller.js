60196a220cf47e6fc175454ac9170b6205243971
Yun YoungSik <ysyun@yuwin.co.kr>
2015-02-18 23:11:48 +0900

    feat(group): add photo-profile directive for profile

--- client/app/group/read/read-group.controller.js
+++ client/app/group/read/read-group.controller.js
@@ -6,32 +6,42 @@
     .controller('ReadGroupCtrl', ReadGroupCtrl);
 
   /* @ngInject */
-  function ReadGroupCtrl($scope, $modalInstance, params, group, filer, sgAlert, logger) {
+  function ReadGroupCtrl($scope, $modalInstance, params, group, sgAlert, logger) {
     $scope.join = join;
     $scope.cancel = cancel;
     _init();
 
     function _init() {
       $scope.group = params.group;
-      $scope.isAdmin = group.isGroupOwner(params.group);
+      // photo-profile directive 에서 사용함 
+      $scope.profile = {
+        modelName: 'groups',
+        modelId: params.group.id,
+        modelPhoto: params.group.photo,
+        isAdmin: group.isGroupOwner(params.group)
+      }
 
-      $scope.$watch('file', function () {
-        if(!$scope.file) { return; }
-        _upload();
-      });
+      // photo-profile directive 로 옮김 
+      // 
+      // $scope.$watch('file', function () {
+      //   if(!$scope.file) { return; }
+      //   _upload();
+      // });
     }
 
-    function _upload() {
-      filer
-        .uploadPhoto('groups', $scope.group.id, $scope.file)
-        .then(function(response) {
-          // change image
-          var media = document.getElementById('_photo');
-          media.src = '/api/v1/groups/' + $scope.group.id + '/photo';  
-        }, function(error) {
-          logger.info('file uploading error: ', error);
-        });
-    }
+    // photo-profile directive 로 옮김 
+    //
+    // function _upload() {
+    //   filer
+    //     .uploadPhoto('groups', $scope.group.id, $scope.file)
+    //     .then(function(response) {
+    //       // change image
+    //       var media = document.getElementById('_photo');
+    //       media.src = '/api/v1/groups/' + $scope.group.id + '/photo';  
+    //     }, function(error) {
+    //       logger.info('file uploading error: ', error);
+    //     });
+    // }
 
     function join() {
 
