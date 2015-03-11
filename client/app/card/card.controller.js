c3bef2a620f9ff8b641ad954b52e4d9794369892
Yun YoungSik <ysyun@yuwin.co.kr>
2015-03-11 20:23:55 +0900

    fix(card): add card.controller.js

---
+++ client/app/card/card.controller.js
@@ -0,0 +1,47 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('surveyGorillaApp')
+    .controller('cardCtrl', cardCtrl);
+
+  /* @ngInject */
+  function cardCtrl($scope, pubsub, modal, logger) {
+    $scope.respondCard = respondCard;
+    $scope.respondMember = respondMember;
+    var vm = this;
+    _init();
+
+    function _init() {
+      _setShow();
+    }
+
+    function _setShow(card) {
+      if(card) {
+        $scope.card = card;
+      }
+      vm.templateId = getTemplateId($scope.card);
+    }
+
+    function getTemplateId(card) {
+      var templateId = '';
+      if(card.type === 'NOTICE') {
+        templateId = 'notice-card.html';
+      } else if(card.type === 'RATING') {
+        templateId = 'rating-card.html';
+      } else if(card.type === 'SURVEY') {
+        if(card.survey.type === 'MULTIPLE_OBJECTIVE') {
+          templateId = 'objective-multi-card.html';
+        } else {
+          templateId = 'objective-card.html';
+        }
+      }  
+      return templateId;
+    }
+
+    function respondCard() {
+      pubsub.publish('response-card:' + $scope.card.id);
+    }
+  }
+
+})();
