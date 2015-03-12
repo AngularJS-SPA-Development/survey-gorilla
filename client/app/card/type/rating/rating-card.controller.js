8ed8a334865257c4817fc7f362951e981951b4f0
Yun YoungSik <ysyun@yuwin.co.kr>
2015-03-12 16:09:10 +0900

    fix(respond): modfiy respond window title

--- client/app/card/type/rating/rating-card.controller.js
+++ client/app/card/type/rating/rating-card.controller.js
@@ -13,7 +13,6 @@
     function _init() {
       _subscribe();
       $scope.ratings = [{id:0, status:false}, {id:1, status:false}, {id:2, status:false}, {id:3, status:false}, {id:4, status:false}];
-
       $scope.response_card = {rating: { rating: 0 }};
     }
 
